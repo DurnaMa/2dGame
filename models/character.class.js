12bdb1e677802a581611553814706cd8e6d68eee
Mahir Durna <durna.m@outlook.de>
2025-04-14 14:54:00 +0200

    Creation of objects in Canvas

--- models/character.class.js
+++ models/character.class.js
@@ -1,11 +1,13 @@
 class Character extends MoveleObjekt {
-  y = 320;
+  y = 270;
   x = 40;
-  height = 500;
-  width = 500;
+  height = 600;
+  width = 580;
 
   constructor() {
     super().loadImage('/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/256x256/rogue.png');
+    this.x = 0, this.x;
+    //this.y = 520 - this.y;
   }
 
   jump() {}
--- models/enemies.class.js
+++ models/enemies.class.js
@@ -1,4 +1,8 @@
 class EnemiesAnt extends MoveleObjekt {
+  y = 350;
+  x = 40;
+  height = 500;
+  width = 500;
   constructor() {
 
     super().loadImage('/assets/2d-pixel-art-evil-monster-sprites/PNG/Big_knight/big_knight01_idle1.png');
