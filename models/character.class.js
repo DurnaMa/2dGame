186f4eda7836dc3597dc9f94caa8461f1e3e4ede
Mahir Durna <durna.m@outlook.de>
2025-04-15 17:38:50 +0200

    movement of hero

--- models/character.class.js
+++ models/character.class.js
@@ -1,13 +1,35 @@
 class Character extends MoveleObjekt {
-  y = 270;
+  y = 420;
   x = 40;
-  height = 600;
-  width = 580;
+  height = 300;
+  width = 300;
+
+  IMAGES_WALKING = [
+    '/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/Walk/walk1.png',
+    '/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/Walk/walk2.png',
+    '/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/Walk/walk3.png',
+    '/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/Walk/walk4.png',
+    '/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/Walk/walk5.png',
+    '/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/Walk/walk6.png',
+  ];
+
+  currentImage = 0;
 
   constructor() {
-    super().loadImage('/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/256x256/rogue.png');
-    this.x = 0, this.x;
-    //this.y = 520 - this.y;
+    super().loadImage('/assets/assassin-mage-viking-free-pixel-art-game-heroes/PNG/Rogue/rogue.png');
+    this.loadImages(this.IMAGES_WALKING);
+    (this.x = 0), this.x;
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let index = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[index];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100 / 0.5);
   }
 
   jump() {}
--- models/world.class.js
+++ models/world.class.js
@@ -1,14 +1,13 @@
 class World {
   character = new Character();
   enemiesAnt = [new EnemiesAnt(), new EnemiesAnt(), new EnemiesAnt()];
-  clouds = [new Cloud(), new Cloud(), new Cloud()];
+  clouds = [new Cloud()];
   backgraundObjecktRocks = [
     new BackgraundObjeckt('assets/mountain-platformer-pixel-art-tileset/PNG/Background/bright/sky.png'),
     new BackgraundObjeckt('assets/mountain-platformer-pixel-art-tileset/PNG/Background/bright/clouds1.png'),
     new BackgraundObjeckt('assets/mountain-platformer-pixel-art-tileset/PNG/Background/bright/clouds2.png'),
     new BackgraundObjeckt('assets/mountain-platformer-pixel-art-tileset/PNG/Background/bright/rocks.png'),
     new BackgraundObjeckt('assets/mountain-platformer-pixel-art-tileset/PNG/Background/bright/rocks2.png'),
-    new BackgraundObjeckt('assets/mountain-platformer-pixel-art-tileset/PNG/Background/bright/clouds3.png'),
     new BackgraundObjeckt('assets/mountain-platformer-pixel-art-tileset/PNG/Background/bright/rocks3.png'),
   ];
   canvas;
@@ -24,7 +23,7 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.addObjectesToMap(this.backgraundObjecktRocks);
-    // this.addObjectesToMap(this.clouds);
+    this.addObjectesToMap(this.clouds);
     this.addToMap(this.character);
     this.addObjectesToMap(this.enemiesAnt);
 
@@ -38,9 +37,9 @@ class World {
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 
-  // addCloudToMap(cloud) {
-  //   this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-  // }
+  addCloudToMap(cloud) {
+    this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
+  }
 
   addObjectesToMap(Objectes) {
     Objectes.forEach((o) => {
