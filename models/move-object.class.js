45de9c7849f699e26d09db2884997896637ed8f9
Mahir Durna <durna.m@outlook.de>
2025-05-14 15:55:51 +0200

    Jump animation images inserted and Gravity

--- models/move-object.class.js
+++ models/move-object.class.js
@@ -8,6 +8,21 @@ class MoveleObjekt {
   currentImage = 0;
   speed = 0.15;
   otherDirection = false;
+  speedY = 0;
+  acceleration = 1;
+
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround() || this.speedY > 0) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      } 
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 420;
+  }
 
   loadImage(path) {
     this.img = new Image();
